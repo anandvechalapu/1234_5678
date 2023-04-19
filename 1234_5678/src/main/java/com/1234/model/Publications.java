83674551cece7f6e4b42ca02f40a7726466d6b06
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-19 16:37:03 +0530

    Updated

--- 1234_5678/src/main/java/com/1234/model/Publications.java
+++ 1234_5678/src/main/java/com/1234/model/Publications.java
@@ -1,63 +1,89 @@
-package com.1234.model;
+@Entity
+package com.1234.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name = "publications")
+@Table(name = "Publications")
 public class Publications {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  @NotEmpty
+  @Column(name = "name")
+  private String name;
+  
+  @Column(name = "publisher")
+  private String publisher;
+  
+  @Column(name = "day")
+  private String day;
+  
+  @Column(name = "active")
+  private boolean active;
+  
+  public Publications(){
     
-    @Column(name = "name")
-    private String name;
-    
-    @Column(name = "days")
-    private String days;
-    
-    @Column(name = "publisher")
-    private String publisher;
-    
-    @Column(name = "active")
-    private boolean active;
-    
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getDays() {
-        return days;
-    }
-    
-    public void setDays(String days) {
-        this.days = days;
-    }
-    
-    public String getPublisher() {
-        return publisher;
-    }
-    
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-    
-    public boolean isActive() {
-        return active;
-    }
-    
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+  }
+  
+  public Publications(String name, String publisher, String day, boolean active){
+    this.name = name;
+    this.publisher = publisher;
+    this.day = day;
+    this.active = active;
+  }
+  
+  public Long getId() {
+    return id;
+  }
+  
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getPublisher() {
+    return publisher;
+  }
+  
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+  
+  public String getDay() {
+    return day;
+  }
+  
+  public void setDay(String day) {
+    this.day = day;
+  }
+  
+  public boolean isActive() {
+    return active;
+  }
+  
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+  
+  @Override
+  public String toString() {
+    return "Publications [id=" + id + ", name=" + name + ", publisher=" + publisher + ", day=" + day + ", active="
+        + active + "]";
+  }
 }
