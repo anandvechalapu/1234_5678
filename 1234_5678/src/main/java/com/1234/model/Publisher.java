b135dca0b539075a8db2d63956ec37a2c57d7c8b
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-19 16:36:45 +0530

    Updated

--- 1234_5678/src/main/java/com/1234/model/Publisher.java
+++ 1234_5678/src/main/java/com/1234/model/Publisher.java
@@ -1,27 +1,25 @@
-package com.1234.model;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="publishers")
 public class Publisher {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     
+    @NotNull
+    @Size(max=50)
     private String name;
+    
+    @NotNull
     private boolean active;
     
-    public Publisher() {}
-    
-    public Publisher(String name, boolean active) {
-        this.name = name;
-        this.active = active;
+    public Publisher() {
     }
     
     public Long getId() {
@@ -47,4 +45,17 @@ public class Publisher {
     public void setActive(boolean active) {
         this.active = active;
     }
+    
+    public void downloadDataAsCsvFormat() {
+        //Download data as .csv format
+    }
+    
+    public void addOrEditPublisher(Publisher publisher) {
+        // Add/Edit the publication
+    }
+    
+    public void resetSelectionToLastSearch(Publisher publisher) {
+        // Reset selection to last search
+    }
+    
 }
