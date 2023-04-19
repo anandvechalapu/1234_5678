f2a48766a09ec3c598a2db96a0481b721f808561
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-19 16:38:05 +0530

    Updated

--- 1234_5678/src/main/java/com/1234/controller/Controller.java
+++ 1234_5678/src/main/java/com/1234/controller/Controller.java
@@ -1,112 +1,94 @@
-package com.1234.controller;
+package com.1234.controller; 
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.1234.model.Publisher;
-import com.1234.model.Publications;
 import com.1234.service.PublisherService;
+import com.1234.entity.Publications;
 import com.1234.service.PublicationsService;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
-	
-	@Autowired
-	private PublisherService publisherService;
-	
-	@Autowired
-	private PublicationsService publicationsService;
-	
-	@GetMapping("/publishers")
-	public List<Publisher> getAllPublishers(){
-		return publisherService.getAllPublishers();
-	}
-	
-	@PostMapping("/publishers")
-	public void addPublisher(@RequestBody Publisher publisher){
-		publisherService.addPublisher(publisher);
-	}
-	
-	@PutMapping("/publishers")
-	public void editPublisher(@RequestBody Publisher publisher){
-		publisherService.editPublisher(publisher);
-	}
-	
-	@GetMapping("/publishers/csv")
-	public List<Object[]> downloadPublishersAsCsv(){
-		return publisherService.downloadPublishersAsCsv();
-	}
-	
-	@GetMapping("/publishers/name/{name}")
-	public Publisher findPublisherByName(@PathVariable String name){
-		return publisherService.findPublisherByName(name);
-	}
-	
-	@GetMapping("/publishers/unique/{name}")
-	public int checkIfPublisherNameUnique(@PathVariable String name){
-		return publisherService.checkIfPublisherNameUnique(name);
-	}
-	
-	@GetMapping("/publishers/active")
-	public List<Publisher> getActivePublishers(){
-		return publisherService.getActivePublishers();
-	}
-	
-	@GetMapping("/publications")
-	public List<Publications> findAll(){
-		return publicationsService.findAll();
-	}
-	
-	@GetMapping("/publications/{id}")
-	public Optional<Publications> findById(@PathVariable Long id){
-		return publicationsService.findById(id);
-	}
-	
-	@GetMapping("/publications/name/{name}")
-	public Publications findByName(@PathVariable String name){
-		return publicationsService.findByName(name);
+ 
+    @Autowired
+    private PublisherService publisherService;
+    
+    @Autowired
+    private PublicationsService publicationsService;
+    
+    @GetMapping("/publishers/name/{name}")
+    public Publisher findByName(@PathVariable String name) {
+        return publisherService.findByName(name);
+    }
+    
+    @GetMapping("/publishers/active/{active}")
+    public Publisher findByActive(@PathVariable boolean active) {
+        return publisherService.findByActive(active);
+    }
+    
+    @GetMapping("/publishers/nameasc")
+    public List<Publisher> findAllByOrderByNameAsc() {
+        return publisherService.findAllByOrderByNameAsc();
+    }
+    
+    @PostMapping("/publishers/download")
+    public void downloadDataAsCsvFormat(){
+        publisherService.downloadDataAsCsvFormat();
+    }
+    
+    @PostMapping("/publishers/addoredit")
+    public void addOrEditPublisher(@RequestBody Publisher publisher) {
+        publisherService.addOrEditPublisher(publisher);
+    }
+    
+    @PostMapping("/publishers/resetselection")
+    public void resetSelectionToLastSearch(@RequestBody Publisher publisher) {
+        publisherService.resetSelectionToLastSearch(publisher);
+    }
+    
+    @GetMapping("/publications/name/{name}")
+    public List<Publications> getPublicationsByName(@PathVariable String name) {
+		return publicationsService.getPublicationsByName(name);
 	}
 	
 	@GetMapping("/publications/publisher/{publisher}")
-	public Publications findByPublisher(@PathVariable String publisher){
-		return publicationsService.findByPublisher(publisher);
+	public List<Publications> getPublicationsByPublisher(@PathVariable String publisher) {
+		return publicationsService.getPublicationsByPublisher(publisher);
 	}
 	
 	@GetMapping("/publications/day/{day}")
-	public Publications findByDay(@PathVariable String day){
-		return publicationsService.findByDay(day);
+	public List<Publications> getPublicationsByDay(@PathVariable String day) {
+		return publicationsService.getPublicationsByDay(day);
 	}
 	
-	@PostMapping("/publications")
-	public Publications save(@RequestBody Publications publication){
-		return publicationsService.save(publication);
+	@GetMapping("/publications/name/{name}/publisher/{publisher}/day/{day}")
+	public List<Publications> getPublicationsByNameAndPublisherAndDay(@PathVariable String name, @PathVariable String publisher, @PathVariable String day) {
+		return publicationsService.getPublicationsByNameAndPublisherAndDay(name, publisher, day);
 	}
 	
-	@DeleteMapping("/publications/{id}")
-	public void deleteById(@PathVariable Long id){
-		publicationsService.deleteById(id);
+	@GetMapping("/publications/active")
+	public List<Publications> getAllActivePublications() {
+		return publicationsService.getAllActivePublications();
 	}
 	
-	@DeleteMapping("/publications")
-	public void deleteAll(){
-		publicationsService.deleteAll();
+	@GetMapping("/publications/inactive")
+	public List<Publications> getAllInActivePublications() {
+		return publicationsService.getAllInActivePublications();
 	}
 	
-	@GetMapping("/publications/csv")
-	public void downloadCsv(){
-		publicationsService.downloadCsv();
+	@GetMapping("/publications/name/{name}/active")
+	public List<Publications> getActivePublicationsByName(@PathVariable String name) {
+		return publicationsService.getActivePublicationsByName(name);
 	}
 	
-	@PutMapping("/publications")
-	public void editPublication(
+	@GetMapping("/publications/name/{name}/publisher/{publisher}/active")
+	public List<Publications> getActivePublicationsByNameAndPublisher(@PathVariable String name, @Path
